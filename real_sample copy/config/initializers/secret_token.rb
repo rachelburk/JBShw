5502bbaf951d72248b357b62f7d86a3fa5aae7b7
Rachel Burkhoff <rachelburkhoff@gmail.com>
2012-06-12 19:22:17 -0400

    W3L03
    Chapter 5

---
+++ real_sample copy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealSample::Application.config.secret_token = 'c054aaed22ae6021d7ee0157a54b8ad5d6dfdcad9debcf300acb132b4ebfb0b640de371270adf4b6c4d2b90497901406b276eea862e7aad4a55bf5f52b982167'
